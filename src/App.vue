d0606a1458c096f021bd3dd1a3dfe68c7d24351f
sunqiwei <18800650372@163.com>
2023-11-17 23:26:43 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
+<!--
+ * @Description: 
+ * @Author: zhuwei
+ * @Date: 2023-11-17 23:16:14
+ * @LastEditors: zhuwei
+ * @LastEditTime: 2023-11-17 23:26:24
+-->
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome1 to Your Vue.js App"/>
 </template>
 
 <script>
